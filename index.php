94448822aa6c9d79970ff84857346217ae284135
Jared Quinn <jared@jaredquinn.info>
2012-04-18 02:16:19 +1000

    Added Analytics.  Added fancy heading.

--- index.php
+++ index.php
@@ -1,9 +1,4 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" >
-<head>
-	<title>Drama.tk</title>
-	<link rel="stylesheet" type="text/css" href="/style.css" />
-	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
-</head>
 <?
 	$str = $_REQUEST['str'] ? $_REQUEST['str'] : "";
 	if(!empty($str)) {
@@ -13,9 +8,28 @@
 	}
 
 ?>
+<head>
+	<title>Drama.tk</title>
+	<link rel="stylesheet" type="text/css" href="/style.css" />
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+	<script type="text/javascript">
+
+	var _gaq = _gaq || []; _gaq.push(['_setAccount', 'UA-30953361-1']); _gaq.push(['_trackPageview']);
+    (function() { var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+			      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+			      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s); })();
+
+<? if(!empty($str)) {  ?>
+		_gaq.push(['_trackEvent', 'Dramatify', 'Text', '<?= urlencode($str) ?>']);
+<? } else { ?>
+		_gaq.push(['_trackEvent', 'Frontpage', '', '']);
+<? } ?>
+  </script>
+</head>
 <body class="<?= $bClass ?>">
-<?
-	if(!empty($str)) {
+
+<?  
+	if(!empty($str)) { 
 		require "dramatk.php";
 	} else {
 		require "input.php";
--- input.php
+++ input.php
@@ -1,6 +1,8 @@
 
 <div class="textbody">
 
+<h1><span class="t1">D</span><span class="t2">r</span><span class="t3">a</span><span class="t4">m</span><span class="t5">a</span><span class="t6">.</span><span class="t7">t</span><span class="t8">k</span></h1>
+
 <form method="post">
 
 	<p>
